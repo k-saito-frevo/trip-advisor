6450cfa1cd0d176dfb327257ac0f4a64d3fd0fc2
k-saito-frevo <k.saito@frevo-works.co.jp>
2019-01-30 18:27:32 +0900

    heroku

--- assets/constants.js
+++ assets/constants.js
@@ -1,6 +1,6 @@
 export default {
     HEADER:{"Access-Control-Allow-Origin":"*",},
-    API_URL:"http://localhost:8080/api/face"
+    API_URL:"https://trip-advisor-server.herokuapp.com/api/face"
     //,
     //API_URL:"https//:google.jp"
 }
